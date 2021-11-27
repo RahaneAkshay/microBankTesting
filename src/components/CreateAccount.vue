68dae8a4a9adf809579517f095108b607adba8d3
Akshay Rahane <v.akshay@validus.sg>
2021-11-27 10:26:54 +0530

    Routing and component added

---
+++ src/components/CreateAccount.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="container">
+    <div class="left-container">
+      <div class="logo-container"></div>
+      <h1>Micro Bank</h1>
+    </div>
+    <div>
+      <h1>Create an Account</h1>
+      <div>
+        <form name="form">
+          <div>
+            <label>Fullname:</label>
+            <input
+              v-model="userFormGroup.props.fullName"
+              type="text"
+              name="name"
+            />
+          </div>
+          <div>
+            <label>Date of incorparation:</label>
+            <input
+              v-model="userFormGroup.props.dateOfIncorporation"
+              type="date"
+              name="date"
+            />
+          </div>
+          <div>
+            <label>Email:</label>
+            <input
+              v-model="userFormGroup.props.email"
+              type="text"
+              name="email"
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              v-model="userFormGroup.props.password"
+              type="password"
+              name="password"
+            />
+          </div>
+          <div>
+            <label>Confirm Password:</label>
+            <input
+              v-model="userFormGroup.props.confirmPassword"
+              type="password"
+              name="confirmpassword"
+            />
+          </div>
+
+          <div>
+            <button
+              type="button"
+              class="btn btn-outline-warning"
+              @click="getFormData()"
+              :disabled="!userFormGroup.valid"
+              id="btn"
+            >
+              <router-link to="/UserDashboard" >Create Account</router-link>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class CreateAccount extends Vue {
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  width: 98%;
+  margin: auto;
+  height: 622px;
+}
+.container > div {
+  width: 50%;
+}
+h1 {
+  margin-top: 40px;
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 32px;
+  line-height: 44px;
+  color: #1a1a1a;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+form > div {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  align-items: flex-start;
+}
+input {
+  width: 320px;
+  height: 50px;
+  background: #e8e8e8;
+  border-radius: 12px;
+  border: none;
+}
+button {
+  border: none;
+  outline: none;
+  width: 320px;
+  height: 50px;
+  background: #d90429;
+  box-shadow: 0px 6px 28px 5px rgba(243, 230, 229, 0.3);
+  border-radius: 12px;
+  color: #ffff;
+}
+.left-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background: #d90429;
+}
+.logo-container {
+  width: 400px;
+  height: 400px;
+  margin: auto;
+  background-image: url("~@/assets/iconn.png");
+}
+.left-container h1 {
+  width: 401px;
+  height: 80px;
+  font-family: Orbitron;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 64px;
+  line-height: 80px;
+  text-align: center;
+  color: #ffffff;
+}
+</style>
---
+++ src/components/UserDashboard.vue
@@ -0,0 +1,87 @@
+/* eslint-disable @typescript-eslint/ban-types */
+<template>
+ <div>
+      <div class="offset-3 col-6"> 
+        <table class="table">
+<thead>
+<tr>
+<th scope="col">Full Name</th>
+<th scope="col">Email</th>
+<th scope="col">Password</th>
+<th scope="col">Edit</th>
+</tr>
+</thead>
+<tbody>
+<tr v-for="(user,index) in userRecords" :key="user.email">
+<th>{{user.fullName}}</th>
+<td>{{user.email}}</td>
+<td>{{user.password}}</td>
+<td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" @click="openUserModal(user,index)">
+  Update
+</button></td>
+</tr>
+</tbody>
+</table>
+  </div>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Update User Data</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+          <form>
+          <div class="mb-3">
+            <label for="fullName" class="col-form-label">fullName:</label>
+            <input type="text" class="form-control" id="fullName" v-model="currentUser.fullName">
+          </div>
+<div class="mb-3">
+            <label for="email" class="col-form-label">Email:</label>
+            <input type="email" class="form-control" id="email" v-model="currentUser.email">
+          </div>
+<div class="mb-3">
+            <label for="password" class="col-form-label">Password:</label>
+            <input type="text" class="form-control" id="password" v-model="currentUser.password">
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="update(currentUser)">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+ </div>
+</template>
+<script lang="ts">
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, Vue } from 'vue-property-decorator';
+@Component({
+  components: {
+  },
+})
+export default class UserDashboard extends Vue {
+  currentUser = {
+    fullName:'',
+    email:'',
+    password:'',
+    index:3
+  }
+
+openUserModal(user:any,index:any):void{
+this.currentUser.fullName = user.fullName
+this.currentUser.email = user.email
+this.currentUser.password = user.password
+this.currentUser.index = index
+}
+
+
+
+}
+
+</script>
+
+<style>
